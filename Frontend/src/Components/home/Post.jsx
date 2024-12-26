19534962e065a87bc10c15bd03d76e928e6e9a89
psychic-coder <rohitganguly450@gmail.com>
2024-12-26 23:45:39 +0530

    completed till post 1

--- Frontend/src/Components/home/Post.jsx
+++ Frontend/src/Components/home/Post.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+import { IoIosMore } from "react-icons/io";
+import Post1 from "./post/Post1";
+import Post2 from "./post/Post2";
 
 const Post = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <>
+      <Stack
+        flexDirection={"row"}
+        justifyContent={"space-between"}
+        borderBottom={"3px solid gray"}
+        p={2}
+        mx={"auto"}
+        width={"70%"}
+        sx={{
+          ":hover": {
+            cursor: "pointer",
+            boxShadow: "10px 10px 10px gray",
+          },
+          transition: "all ease-in-out 0.3s",
+        }}
+      >
+        <Stack flexDirection={"row"} gap={2}>
+          <Post1/>
+          <Post2/>
+        </Stack>
+        <Stack
+          flexDirection={"row"}
+          justifyContent={"center"}
+          gap={1}
+          fontSize={"1rem"}
+        >
+          <Typography
+            variant="caption"
+            color={"GrayText"}
+            fontSize={"1rem"}
+            top={2}
+            position={"relative"}
+          >
+            23 hrs
+          </Typography>
+          <IoIosMore  size={28}/>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
 
-export default Post
+export default Post;
---
+++ Frontend/src/Components/home/post/Post2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Post2 = () => {
+  return (
+    <div>Post2</div>
+  )
+}
+
+export default Post2
