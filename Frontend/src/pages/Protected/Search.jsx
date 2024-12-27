3dfba4f2accf44c8d6b6ff3c9e64001ec688b9ab
psychic-coder <rohitganguly450@gmail.com>
2024-12-27 15:51:17 +0530

    search completed

--- Frontend/src/pages/Protected/Search.jsx
+++ Frontend/src/pages/Protected/Search.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import SearchInput from '../../Components/search/SearchInput'
+import ProfileBar from '../../Components/search/ProfileBar'
+import { Stack } from '@mui/material'
 
 const Search = () => {
   return (
     <div>
       <SearchInput/>
-
-      {/* profilebar */}
+      <Stack flexDirection={"column"} gap={1} mb={5} width={"50%"} maxWidth={'750px'} mx={'auto'}> 
+      <ProfileBar/>
+      <ProfileBar/>
+      <ProfileBar/>
+      </Stack>
+    
     </div>
   )
 }
