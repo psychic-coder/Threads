6a1d526be4a5039db7bd5f49a45bee1d0fe92245
psychic-coder <rohitganguly450@gmail.com>
2024-12-28 23:51:52 +0530

    profile layout responsve

--- Frontend/src/pages/Protected/profile/ProfileLayout.jsx
+++ Frontend/src/pages/Protected/profile/ProfileLayout.jsx
@@ -1,14 +1,19 @@
-import { Avatar, Button, Chip, Stack, Typography, useMediaQuery } from "@mui/material";
-import {Link, Outlet} from "react-router-dom"
+import {
+  Avatar,
+  Button,
+  Chip,
+  Stack,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
 
 const ProfileLayout = () => {
-
-    const _700=useMediaQuery("(min-width:700px)");
-    const _500=useMediaQuery("(min-width:500px)");
-    const _300=useMediaQuery("(min-width:300px)");
-
+  const _700 = useMediaQuery("(min-width:700px)");
+  const _500 = useMediaQuery("(min-width:500px)");
+  const _300 = useMediaQuery("(min-width:300px)");
 
   return (
     <>
@@ -17,7 +22,7 @@ const ProfileLayout = () => {
         gap={2}
         p={2}
         m={2}
-        width={ "800px"}
+        width={_700 ? "800px" : "90%"}
         mx={"auto"}
       >
         <Stack
@@ -29,25 +34,25 @@ const ProfileLayout = () => {
             <Typography
               variant="h2"
               fontWeight={"bold"}
-              fontSize={ "1rem"}
+              fontSize={_300 ? "2rem" : "1rem"}
             >
               Lorem, ipsum.
             </Typography>
             <Stack flexDirection={"row"} alignItems={"center"} gap={1}>
-              <Typography variant="h2" fontSize={"1rem" }>
+              <Typography variant="h2" fontSize={_300 ? "2rem" : "0.8rem"}>
                 Lorem, ipsum.
               </Typography>
               <Chip
                 label="threads.net"
                 size="small"
-                sx={{ fontSize:  "0.6rem" }}
+                sx={{ fontSize: _300 ? "0.8rem" : "0.6rem" }}
               />
             </Stack>
           </Stack>
           <Avatar
-            src={ ""}
+            src={""}
             alt={""}
-            sx={{ width:60, height:60  }}
+            sx={{ width: _300 ? 60 : 40, height: _300 ? 60 : 40 }}
           />
         </Stack>
         <Typography variant="subtitle2">Lorem ipsum dolor sit.</Typography>
@@ -59,14 +64,14 @@ const ProfileLayout = () => {
           <Typography variant="subtitle2" color="gray">
             19 Followers
           </Typography>
-          <FaInstagram size={40} />
+          <FaInstagram size={_300?40:24} />
         </Stack>
       </Stack>
       <Button
         size={"large"}
         sx={{
           color: "black",
-          width: "800px",
+          width: _700?"800px":"90%",
           mx: "auto",
           textAlign: "center",
           border: "1px solid gray",
@@ -84,15 +89,21 @@ const ProfileLayout = () => {
         my={5}
         pb={2}
         borderBottom={"2px solid gray"}
-        fontSize={"1.2rem"}
-        width={"800px"}
+        fontSize={_500?"1.2rem":(_300?"1.1rem":"0.9rem")}
+        width={_700?"800px":"90%"}
         mx={"auto"}
       >
-        <Link to={"/profile/threads/1"} className="Link">Threads</Link>
-        <Link to={"/profile/replies/1"} className="Link">Replies</Link>
-        <Link to={"/profile/reposts/1"} className="Link">Reposts</Link>
+        <Link to={"/profile/threads/1"} className="Link">
+          Threads
+        </Link>
+        <Link to={"/profile/replies/1"} className="Link">
+          Replies
+        </Link>
+        <Link to={"/profile/reposts/1"} className="Link">
+          Reposts
+        </Link>
       </Stack>
-      <Outlet/>
+      <Outlet />
     </>
   );
 };
